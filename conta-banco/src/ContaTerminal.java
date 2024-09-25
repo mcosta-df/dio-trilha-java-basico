25f9a5a0b720f4692c063f93ee32704cdf08cbaa
mcosta-df <mcosta.df@gmail.com>
2024-09-24 22:55:24 -0300

    Conta terminal implementada

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        Scanner input = new Scanner(System.in);
+
+       int numeroConta;
+       String agencia;
+       String nomeCliente;
+       Float saldo;
+
+       System.out.print("Digite no numero da agência(XXX-X): ");
+       agencia = input.next();
+
+       System.out.print("Digite o numero da conta: ");
+       numeroConta = input.nextInt();
+
+       System.out.print("Diginte o nome do cliente: ");
+       nomeCliente = input.next();
+
+       System.out.print("Digite o saldo: ");
+       saldo = input.nextFloat();
+
+        System.out.printf("Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %s, conta %d e seu saldo %5.2f já está disponível para saque", nomeCliente, agencia, numeroConta, saldo);
     }
 }
