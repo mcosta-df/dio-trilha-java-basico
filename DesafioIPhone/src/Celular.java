4f44ce4312c0ca38a1d0f8b742ea8b5a90ef9e00
mcosta-df <mcosta.df@gmail.com>
2024-09-30 22:51:37 -0300

    Desafio IPhone

---
+++ DesafioIPhone/src/Celular.java
@@ -0,0 +1,17 @@
+public class Celular implements AparelhoTelefonico{
+    
+    @Override
+    public void ligar(String numero){
+        System.out.println("ligando para o numero especificado");
+    }
+
+    @Override
+    public void atender(){
+        System.out.println("Atendendo uma ligacao");
+    }
+
+    @Override
+    public void iniciarCorreioVoz()(
+        System.out.println("Iniciando um correio de voz");
+    )
+}
